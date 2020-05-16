1757bef7fe1b44396eb4325dcd54a506b86a597e
enwee <tanenwee@gmail.com>
2020-05-17 06:33:16 +0800

    Add puzzle hint, improve mouse/touch handling, random scattering

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 import jigsaw from "./jigsaw.js";
 
+const bgColor = ["teal", "purple", "brown", "steelblue"];
+const images = ["stamp", "hand", "greedy", "water"];
+const randomInt = (range) => Math.floor(Math.random() * range);
+
 let imgSrc = image.files[0]
   ? URL.createObjectURL(image.files[0])
-  : "./stamp.jpg";
+  : `./images/${images[randomInt(images.length)]}.jpg`;
 let cols = columns.value || 6;
 let puzzleWidthPx = width.value || 600;
 let flag = false;
-const bgColor = ["teal", "purple", "brown", "plum"];
 
-// const image = document.getElementById("image");
-// const columns = document.getElementById("columns");
-// const width = document.getElementById("width");
-// const play = document.getElementById("play");
-// const board = document.getElementById("board");
+const initRnd = randomInt(4);
+document.body.style.background = bgColor[initRnd];
 
 image.onchange = () => {
   imgSrc = URL.createObjectURL(image.files[0]);
-  drawPuzzle();
+  startGame();
 };
 
 columns.onchange = () => {
   cols = columns.value;
-  drawPuzzle();
+  startGame();
 };
 
 width.onchange = () => {
   puzzleWidthPx = width.value;
-  drawPuzzle();
+  startGame();
 };
 
 play.onclick = () => {
   flag = !flag;
   play.innerText = flag ? "Solve!" : "Scatter!";
-  drawPuzzle();
+  startGame();
 };
 
-const drawPuzzle = async () => {
+const startGame = async () => {
   while (board.firstChild) {
     board.removeChild(board.lastChild);
   }
@@ -44,71 +44,89 @@ const drawPuzzle = async () => {
       = await jigsaw(imgSrc, cols, puzzleWidthPx).catch(alert);
   board.style.width = `${boardSize.X + tileSize.X / 2}px`;
   board.style.height = `${boardSize.Y + tileSize.Y / 2}px`;
-  board.style.outlineOffset = `-${tileSize.X / 4 + 1}px`;
+  board.style.outlineOffset = `-${(tileSize.X + tileSize.Y) / 8 + 2}px`;
 
   const completed = Array(tilePicArray.length).fill(false);
   let isDragging = false;
-  let z = 1;
+  let z = tilePicArray.length;
+
+  const img = new Image();
+  img.src = imgSrc;
+  img.style.width = `${boardSize.X}px`;
+  img.style.height = `${boardSize.Y}px`;
+  img.style.left = `${tileSize.X / 4}px`;
+  img.style.top = `${tileSize.Y / 4}px`;
+  img.className = "hint";
+  img.draggable = false;
+  board.appendChild(img);
 
   tilePicArray.forEach((tile, index) => {
-    const randomSide = Math.random() < 0.5;
-    const randomX = randomSide
-      ? Math.floor((Math.random() * tileSize.X) / 2) + boardSize.X
-      : Math.floor(Math.random() * boardSize.X);
-    const randomY = randomSide
-      ? Math.floor(Math.random() * boardSize.Y)
-      : Math.floor((Math.random() * tileSize.Y) / 2) + boardSize.Y;
+    const side = randomInt(2);
+    const scatterArea = [
+      {
+        x: randomInt(boardSize.X - tileSize.X),
+        y: randomInt(tileSize.Y * 1.5) + boardSize.Y + tileSize.Y / 4,
+      },
+      {
+        x: randomInt(tileSize.X * 1.5) + boardSize.X + tileSize.X / 4,
+        y: randomInt(boardSize.Y - tileSize.Y),
+      },
+    ];
 
     const img = new Image();
     img.src = tile.base64Url;
-    img.style.left = flag ? `${randomX}px` : `${tile.leftOffset}px`;
-    img.style.top = flag ? `${randomY}px` : `${tile.topOffset}px`;
-    img.style.userSelect = "none";
+    img.style.left = flag ? `${scatterArea[side].x}px` : `${tile.leftOffset}px`;
+    img.style.top = flag ? `${scatterArea[side].y}px` : `${tile.topOffset}px`;
+    img.style.zIndex = randomInt(tilePicArray.length);
     img.className = "tile";
-    img.draggable = false;
-
     board.appendChild(img);
 
-    const moveTile = (x, y) => {
-      img.style.left = `${x - tileSize.X * 0.75}px`;
-      img.style.top = `${y - tileSize.Y * 0.75}px`;
-      img.style.zIndex = z;
+    const moveTile = (pageX, pageY) => {
+      img.style.left = `${pageX - tileSize.X * 0.75 - board.offsetLeft}px`;
+      img.style.top = `${pageY - tileSize.Y * 0.75 - board.offsetTop}px`;
     };
 
-    const dropTile = (x, y) => {
-      const X = x - tileSize.X / 4;
-      const Y = y - tileSize.Y / 4;
-      z++;
-
-      if (X > 0 && X < boardSize.X && Y > 0 && Y < boardSize.Y) {
-        img.style.left = `${Math.trunc(X / tileSize.X) * tileSize.X}px`;
-        img.style.top = `${Math.trunc(Y / tileSize.Y) * tileSize.Y}px`;
-      }
-
-      completed[index] =
-        parseInt(img.style.left) === tile.leftOffset &&
-        parseInt(img.style.top) === tile.topOffset;
-      if (completed.every((tile) => tile)) {
-        flag = false;
-        play.innerText = "Play!";
-        completed.fill(false);
-        bgColor.push(bgColor.shift());
-        document.body.style.background = bgColor[0];
+    const dropTile = (pageX, pageY) => {
+      const x = pageX - tileSize.X / 4 - board.offsetLeft;
+      const y = pageY - tileSize.Y / 4 - board.offsetTop;
+      if (x > 0 && x < boardSize.X && y > 0 && y < boardSize.Y) {
+        img.style.left = `${Math.trunc(x / tileSize.X) * tileSize.X}px`;
+        img.style.top = `${Math.trunc(y / tileSize.Y) * tileSize.Y}px`;
+        completed[index] =
+          parseInt(img.style.left) === tile.leftOffset &&
+          parseInt(img.style.top) === tile.topOffset;
+        if (completed.every((tile) => tile)) {
+          flag = false;
+          play.innerText = "Play!";
+          completed.fill(false);
+          bgColor.push(bgColor.shift());
+          document.body.style.background = bgColor[initRnd];
+        }
       }
     };
 
     img.onmousedown = (e) => {
+      e.preventDefault(); //cos ff draggable="false" !working
       isDragging = true;
+      moveTile(e.pageX, e.pageY);
+      img.style.zIndex = z++;
     };
 
     img.onmousemove = (e) => {
       if (isDragging) moveTile(e.pageX, e.pageY);
     };
 
+    img.onmouseleave = () => (isDragging = false);
+
     img.onmouseup = (e) => {
       isDragging = false;
       dropTile(e.pageX, e.pageY);
     };
+    //prettier-ignore
+    img.addEventListener("touchstart", (e) => {
+      moveTile(e.touches[0].pageX, e.touches[0].pageY);
+      img.style.zIndex = z++;
+    }, {passive: true});
 
     img.ontouchmove = (e) => {
       e.preventDefault();
@@ -121,4 +139,4 @@ const drawPuzzle = async () => {
   });
 };
 
-drawPuzzle();
+startGame();
